502106967981e783c14467d12d5d4d03e67c0c03
RoSpr <rsprikut@mail.ru>
2020-11-18 00:32:29 +0500

    Added main files

---
+++ NIT_2.3/NIT_2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NIT_2.3
+//
+//  Created by Родион Сприкут on 17.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
